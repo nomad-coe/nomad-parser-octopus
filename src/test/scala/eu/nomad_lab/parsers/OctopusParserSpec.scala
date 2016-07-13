8eca1a42f172067f28904a7584e2592a5584cd1c
Ask Hjorth Larsen <asklarsen@gmail.com>
2016-07-13 14:53:36 +0200

    enable more tests

--- src/test/scala/eu/nomad_lab/parsers/OctopusParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/OctopusParserSpec.scala
@@ -10,5 +10,29 @@ object OctopusParserSpec extends Specification {
     "test with json" >> {
       ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/ink-gs/static/info", "json") must_== ParseResult.ParseSuccess
     }
+    "test H2O with json-events" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/H2O/static/info", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test H2O with json" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/H2O/static/info", "json") must_== ParseResult.ParseSuccess
+    }
+    "test O2 with json-events" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/O2/static/info", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test O2 with json" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/O2/static/info", "json") must_== ParseResult.ParseSuccess
+    }
+    "test Si with json-events" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/Si/static/info", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test Si with json" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/Si/static/info", "json") must_== ParseResult.ParseSuccess
+    }
+    "test Fe with json-events" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/Fe/static/info", "json-events") must_== ParseResult.ParseSuccess
+    }
+    "test Fe with json" >> {
+      ParserRun.parse(OctopusParser, "parsers/octopus/test/examples/Fe/static/info", "json") must_== ParseResult.ParseSuccess
+    }
   }
 }
