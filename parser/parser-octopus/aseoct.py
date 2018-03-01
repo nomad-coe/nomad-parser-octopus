f1ee8be9511cc25d94d10cd35f0535d565ebe4b6
Ask Hjorth Larsen <asklarsen@gmail.com>
2018-03-01 20:28:13 +0100

    add license headers

--- parser/parser-octopus/aseoct.py
+++ parser/parser-octopus/aseoct.py
@@ -1,19 +1,25 @@
-# coding=utf-8
-"""This is based on a fork of the ASE-interface to Octopus.
+# Copyright 2016-2018 The NOMAD Developers Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-The Octopus ASE interface is written by
+# coding = utf-8
+
+"""This module is written by
 
   Ask Hjorth Larsen <asklarsen@gmail.com>
   Carlos de Armas
-
-and made available for NOMAD to license
-as appropriate.
-
-It has *also* been contributed to ASE itself.
-The copy within ASE is under LGPL v2.1+.
-
-http://tddft.org/programs/octopus/
 """
+
 import os
 import re
 from subprocess import Popen, PIPE
