c3e8c07440cf7c7cd9f55c363c75d23c249a8a83
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-04-30 19:51:27 +0200

    Corrected tests

--- tests/test_octopusparser.py
+++ tests/test_octopusparser.py
@@ -23,6 +23,10 @@ from nomad.datamodel import EntryArchive
 from octopusparser import OctopusParser
 
 
+def approx(value, abs=0, rel=1e-6):
+    return pytest.approx(value, abs=abs, rel=rel)
+
+
 @pytest.fixture(scope='module')
 def parser():
     return OctopusParser()
@@ -34,7 +38,7 @@ def test_scf(parser):
 
     sec_run = archive.section_run[0]
     assert sec_run.program_version == 'wolfi'
-    assert sec_run.x_octopus_input_Spacing == pytest.approx(0.28345892)
+    assert sec_run.x_octopus_input_Spacing == approx(0.28345892)
     assert sec_run.x_octopus_parserlog_SpeciesProjectorSphereThreshold == 0.001
 
     sec_method = sec_run.section_method[0]
@@ -45,20 +49,20 @@ def test_scf(parser):
     sec_system = sec_run.section_system[0]
     assert False not in sec_system.configuration_periodic_dimensions
     assert sec_system.atom_labels == ['Si', 'Si', 'Si', 'Si']
-    assert sec_system.atom_positions[1][0].magnitude == pytest.approx(1.91979671e-10)
+    assert sec_system.atom_positions[1][0].magnitude == approx(1.91979671e-10)
 
     sec_scc = sec_run.section_single_configuration_calculation[0]
-    assert sec_scc.energy_total.magnitude == pytest.approx(-6.91625667e-17)
-    assert sec_scc.energy_electrostatic.magnitude == pytest.approx(4.79087203e-18)
+    assert sec_scc.energy_total.magnitude == approx(-6.91625667e-17)
+    assert sec_scc.energy_electrostatic.magnitude == approx(4.79087203e-18)
     assert np.count_nonzero(sec_scc.atom_forces_free_raw) == 0
     sec_eig = sec_scc.section_eigenvalues[0]
     assert np.shape(sec_eig.eigenvalues_values) == (1, 18, 8)
     assert sec_eig.eigenvalues_kpoints[11][2] == 0.25
-    assert sec_eig.eigenvalues_values[0][4][6].magnitude == pytest.approx(5.26639723e-19)
+    assert sec_eig.eigenvalues_values[0][4][6].magnitude == approx(5.26639723e-19)
     assert sec_eig.eigenvalues_occupation[0][16][1] == 2.0
     sec_scf = sec_scc.section_scf_iteration
     assert len(sec_scf) == 8
-    assert sec_scf[3].energy_total_scf_iteration.magnitude == pytest.approx(-6.91495422e-17)
+    assert sec_scf[3].energy_total_scf_iteration.magnitude == approx(-6.91495422e-17)
     assert sec_scf[7].time_scf_iteration.magnitude == 9.42
 
 
@@ -71,15 +75,15 @@ def test_spinpol(parser):
     assert archive.section_run[0].x_octopus_parserlog_PreconditionerFilterFactor == 0.6
 
     sec_scc = archive.section_run[0].section_single_configuration_calculation[0]
-    assert sec_scc.energy_reference_fermi[0].magnitude == pytest.approx(7.39160185e-19)
+    assert sec_scc.energy_reference_fermi[0].magnitude == approx(7.39160185e-19)
     sec_eig = sec_scc.section_eigenvalues[0]
     assert np.shape(sec_eig.eigenvalues_values) == (2, 10, 20)
-    assert sec_eig.eigenvalues_values[1][5][4].magnitude == pytest.approx(-7.40576381e-18)
+    assert sec_eig.eigenvalues_values[1][5][4].magnitude == approx(-7.40576381e-18)
     assert sec_eig.eigenvalues_kpoints[9][0] == 0.5
     assert sec_eig.eigenvalues_occupation[0][1][17] == 0.972222
     sec_scfs = sec_scc.section_scf_iteration
-    assert sec_scfs[0].energy_total_scf_iteration.magnitude == pytest.approx(-1.02450582e-15)
-    assert sec_scfs[5].energy_reference_fermi_iteration[0].magnitude == pytest.approx(7.85685151e-19)
+    assert sec_scfs[0].energy_total_scf_iteration.magnitude == approx(-1.02450582e-15)
+    assert sec_scfs[5].energy_reference_fermi_iteration[0].magnitude == approx(7.85685151e-19)
     assert sec_scfs[8].time_scf_iteration.magnitude == 15.63
 
 
